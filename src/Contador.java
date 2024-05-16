eafcffffbff15f03f5321a43a5988b95302e5049
Juliana Prado <ju.pradoo@gmail.com>
2024-05-16 17:46:28 -0300

    Desafio Java basico - controle de fluxo

---
+++ src/Contador.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro parâmetro ");
+        int parametroUm = terminal.nextInt();
+        System.out.print("Digite o segundo parâmetro ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("Segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+
+        if (parametroUm < parametroDois) {
+            int contagem = parametroDois - parametroUm;
+
+            for(int i = 0; i < contagem; i++) {
+                System.out.println("Imprimindo o numero " + (i + 1));
+            }
+        }
+    }
+}
